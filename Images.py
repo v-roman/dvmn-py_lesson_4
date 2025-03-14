55b768f1e1091c6f56de97aaaf475ea16bd780c6
v-roman <r.v.v.05112001@gmail.com>
2025-03-14 16:24:56 +0300

    Add files via upload

---
+++ Images.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+image_monro = Image.open("monro.jpg")
+red_channel, green_channel, blue_channel = image_monro.split()
+red_channel_middle = red_channel.crop((50, 0, 646, 522))
+red_channel_left = red_channel.crop((100, 0, red_channel.width, red_channel.height))
+shift_channel_red = Image.blend(red_channel_middle, red_channel_left, 0.5)
+blue_channel_middle = blue_channel.crop((50, 0, 646, 522))
+red_channel_right = blue_channel.crop((0, 0, 596, 522))
+shift_channel_blue = Image.blend(blue_channel_middle, red_channel_right, 0.5)
+green_channel_middle = green_channel.crop((50, 0, 646, 522))
+image_monro_crop = Image.merge("RGB", (shift_channel_red, shift_channel_blue, green_channel_middle))
+image_monro_crop.save('image_monro_crop.jpg')
+image_monro_crop.thumbnail((80, 80))
+image_monro_crop.save('image_monro_thumb.jpg')
+
